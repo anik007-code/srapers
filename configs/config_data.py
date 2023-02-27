944268541ab07851814e3134d8d0e09da7ceee0d
Anik <aniklpu01@gmail.com>
2023-02-27 23:48:46 +0600

    features added

---
+++ configs/config_data.py
@@ -0,0 +1,13 @@
+import os
+
+SITE_LINK = "https://www.amazon.com/s?k=smartphones&crid=396MZ2GWQQSNF&sprefix=smartphon%2Caps%2C1164&ref=nb_sb_noss_2"
+ROOT_PATH = os.getcwd()
+DATA_PATH = "DATA"
+DRIVER_PATH = "DRIVER"
+INFO_PATH = "INFO"
+FILE = "data.json"
+FILE_PATH = "INFO"
+MINI_WAIT = 1
+WAIT = 20
+MEDIUM_WAIT = 120
+LONG_WAIT = 240
