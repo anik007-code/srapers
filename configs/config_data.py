4d97248edbaeeb2034c8d3d978436c9d6b9d1331
Anik <aniklpu01@gmail.com>
2023-05-08 15:50:36 +0600

    scraper is not completed yet

---
+++ configs/config_data.py
@@ -0,0 +1,13 @@
+import os
+
+SITE_LINK = "http://bangladeshyellowpages.com/business.html"
+ROOT_PATH = os.getcwd()
+DATA_PATH = "DATA"
+DRIVER_PATH = "DRIVER"
+INFO_PATH = "INFO"
+FILE = "data.json"
+FILE_PATH = "INFO"
+MINI_WAIT = 5
+WAIT = 20
+MEDIUM_WAIT = 120
+LONG_WAIT = 240
